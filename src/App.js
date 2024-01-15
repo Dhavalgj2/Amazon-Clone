11ba78a878efbfb1a06b116e05f29a7d3a033ac2
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2024-01-15 18:53:55 +1100

    header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import Home from "./Components/Main/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home></Home>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../../assets/logo.svg";
 import styles from "./Header.module.scss";
 import Search from "../Search/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 const Header = () => {
   return (
     <div className={styles.root}>
@@ -10,6 +12,23 @@ const Header = () => {
           <img src={logo} alt="" width="90px" height="100px" />
         </div>
         <Search />
+        <div className="header-options-container">
+          <div className="header-options">
+            <span>Hello, User</span>
+          </div>
+          <div className="header-options">
+            <span>
+              Returns
+              <b> & Orders</b>
+            </span>
+          </div>
+          <div className="header-options">
+            <span className="cart-container">
+              <ShoppingCartIcon fontSize="small" />
+              <span>0</span>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
